881735dec952117ef3763b67fdfaa66ab924314f
akshat.thakar <akshat.thakar@gmail.com>
2026-05-08 20:02:27 +0530

    Save settings changes

--- django_app/settings.py
+++ django_app/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-me'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['akshatapp-axfvcpa5f4d2bmgm.canadacentral-01.azurewebsites.net','localho']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
